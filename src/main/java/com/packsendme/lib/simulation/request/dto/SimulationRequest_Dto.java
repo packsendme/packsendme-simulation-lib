83c0fb133ed6af11a2b146a9a3931665cc8cb29c
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2020-05-08 14:11:58 +0200

    NewVersion/0.2.0

--- src/main/java/com/packsendme/lib/simulation/request/dto/SimulationRequest_Dto.java
+++ src/main/java/com/packsendme/lib/simulation/request/dto/SimulationRequest_Dto.java
@@ -15,7 +15,6 @@ public class SimulationRequest_Dto implements Serializable {
 	public static final long serialVersionUID = 1L;
 	public String address_origin;
 	public String address_destination;
-	public double distances_places;
 	public String type_product;
 	public double weight_product;
 	public String type_delivery;
