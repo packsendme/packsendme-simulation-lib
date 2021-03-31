7f4bf87d2c8f52a9c4cd2fddfb3831c083423ce3
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2021-03-31 19:23:13 +0200

    OK

--- src/main/java/com/packsendme/lib/roadway/simulation/request/SimulationRoadwayRequest_Dto.java
+++ src/main/java/com/packsendme/lib/roadway/simulation/request/SimulationRoadwayRequest_Dto.java
@@ -31,14 +31,14 @@ public class SimulationRoadwayRequest_Dto implements Serializable{
 	public Double length_max;
 	public String delivery_type;
 	public String language_locale;
-	public String language_country;
+	public String country_locale;
 	public Exchange exchangeObj;
 	public GoogleAPITrackingResponse_Dto googleTracking;
 	public Roadway roadwayRule;
 	
 	public SimulationRoadwayRequest_Dto(String address_origin, String address_destination, String type_transport,
 			String product_transport, int people, Double weight_max, String unity_weight, Double height_max,
-			Double width_max, Double length_max, String delivery_type, String language_locale, String language_country,
+			Double width_max, Double length_max, String delivery_type, String language_locale, String country_locale,
 			Exchange exchangeObj, GoogleAPITrackingResponse_Dto googleTracking, Roadway roadwayRule) {
 		super();
 		this.address_origin = address_origin;
@@ -53,7 +53,7 @@ public class SimulationRoadwayRequest_Dto implements Serializable{
 		this.length_max = length_max;
 		this.delivery_type = delivery_type;
 		this.language_locale = language_locale;
-		this.language_country = language_country;
+		this.country_locale = country_locale;
 		this.exchangeObj = exchangeObj;
 		this.googleTracking = googleTracking;
 		this.roadwayRule = roadwayRule;
